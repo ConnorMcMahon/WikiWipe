29b67846b922c9b33b581f3c1a1d356fd96f3787
Connor McMahon <connorm333@gmail.com>
2016-02-09 15:14:36 -0600

    Added functionality to remove factoids and wikipedia links

--- wikipurge.js
+++ wikipurge.js
@@ -1,26 +1,55 @@
-var elemParent = document.body
+const ELEMENT_PARENT = document.body
+
+const KNOWLEDGE_BOX_CLASS = 'g mnr-c rhsvw kno-kp g-blk'
+const FACTOID_CLASS = 'g mnr-c g-blk'
+const SEARCH_RESULTS_CLASS = 'rc'
+
+const WIKI_REGEX = /.*\.wikipedia\.org.*/
+
+var foundWikiData = false;
+var loggedSearchInfo = false;
 
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         if (mutation.addedNodes && (mutation.addedNodes.length > 0)) {
-            var knowledgeBoxRemoved = false;
-            
-            //locates any node with the magic class name as a knowledge box
-            var knowledgeBoxes = document.getElementsByClassName("g mnr-c rhsvw kno-kp g-blk");
+            //locates any potential dom elements to remove
+            var knowledgeBoxes = document.getElementsByClassName(KNOWLEDGE_BOX_CLASS);
+            var factoids = document.getElementsByClassName(FACTOID_CLASS);
+            var searchResults = document.getElementsByClassName(SEARCH_RESULTS_CLASS);
             
             if (knowledgeBoxes) {
                 for(var i = 0; i < knowledgeBoxes.length; i++){
+                    //hides the knowledge box
                     knowledgeBoxes[i].style.setProperty('display', 'none', 'important');
-                    //console.log("hid element");
                 }
-                knowledgeBoxRemoved = true;
+                foundWikiData = true;
             }
 
-            if (knowledgeBoxRemoved) {
-                //The below line makes more efficient, but stops subsequent searches from blocking
-                //observer.disconnect();
+            if (factoids) {
+                for(var i = 0; i < factoids.length; i++){
+                    //hides the factoid
+                    factoids[i].style.setProperty('display', 'none', 'important');
+                }
+                foundWikiData = true;
+            }
 
+            if (searchResults) {
+                for(var i = 0; i < searchResults.length; i++){
+                    //finds the link of the search result
+                    var linkName = searchResults[i].childNodes[0].childNodes[0].href
+                    //determines if link is from wikipedia using regex
+                    var isWikiLink = WIKI_REGEX.test(linkName);
+                    //hides the link if it is from wikipedia
+                    if (isWikiLink){
+                        searchResults[i].style.setProperty('display', 'none', 'important');
+                        foundWikiData = true;
+                    }
+                }
+            }
+
+            if (foundWikiData && !loggedSearchInfo) {
                 //TODO: add some logging features here
+                loggedSearchInfo = true;
             }
         }
     });
@@ -28,7 +57,7 @@ var observer = new MutationObserver(function(mutations) {
 
 
 
-observer.observe(elemParent, {
+observer.observe(ELEMENT_PARENT, {
     childList: true,
     subtree: true
 });
