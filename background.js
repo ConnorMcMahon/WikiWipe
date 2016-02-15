5daf8ea80d2fc4382ab2c9a3fbfd13848ddb7e50
Connor McMahon <connorm333@gmail.com>
2016-02-15 15:23:04 -0600

    Added icon visual as to whether extension is on or off

--- background.js
+++ background.js
@@ -1,7 +1,21 @@
 var extensionOn = true;
 
+var getIcon = function(extensionOn){
+	var path;
+	if(extensionOn){
+		path = "icon.png"
+	} else {
+		path = "icon2.png"
+	}
+	return path;
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 	extensionOn = !extensionOn;
+	iconPath = getIcon(extensionOn);
+	chrome.browserAction.setIcon({
+		path: iconPath
+	});
 });
 
 chrome.runtime.onMessage.addListener(function (req, send, sendResponse) {
