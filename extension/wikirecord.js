bea69bad2e727c110c96c18a5fd65cf34510b74e
Connor McMahon <connorm333@gmail.com>
2016-03-03 15:00:08 -0600

    Fixed logging logic in wiki sessions

--- extension/wikirecord.js
+++ extension/wikirecord.js
@@ -7,35 +7,39 @@ userID = 10;
 
 console.log(logEntry);
 //updates the database with this new log
-// jQuery.ajax({
-//     type: "GET",
-//     url: SERVER + "/getLatestWikiSession/?id=" + userID,
-//     success: function(data) {
-//         if (data) {
-//             //grab last time this session was updated, and starts a new session if it has expired
-//             var lastSessionTime = data.logs.slice(-1)[0].timestamp;
-//             if ((logEntry.timestamp - lastSessionTime) > SESSION_TIMEOUT){
-//                 data.logs = [logEntry];
-//                 data.sessionID += 1
-//             } else {
-//                 data.logs.push(logEntry);
-//             }
-//         } else {
-//             data = {
-//                 "userID": userID,
-//                 "sessionID": 1,
-//                 "logs": [logEntry]
-//             }
-//         }
+jQuery.ajax({
+    type: "GET",
+    url: SERVER + "/getLatestWikiSession/?id=" + userID,
+    success: function(data) {
+        if (data) {
+            if(data.logs) {
+                //grab last time this session was updated, and starts a new session if it has expired
+                var lastSessionTime = data.logs.slice(-1)[0].timestamp;
+                if ((logEntry.timestamp - lastSessionTime) > SESSION_TIMEOUT){
+                    data.logs = [logEntry];
+                    data.sessionID += 1
+                } else {
+                    data.logs.push(logEntry);
+                }
+            } else {
+                data.logs = [logEntry];
+            }
+        } else {
+            data = {
+                "userID": userID,
+                "sessionID": 1,
+                "logs": [logEntry]
+            }
+        }
 
 
-//         jQuery.ajax({
-//             type: "POST",
-//             url: SERVER + "/addWikiLog",
-//             data: data,
-//             success: function(data){
-//                 querySent = true;
-//             }
-//         });
-//     }
-// });
+        jQuery.ajax({
+            type: "POST",
+            url: SERVER + "/addWikiLog",
+            data: data,
+            success: function(data){
+                querySent = true;
+            }
+        });
+    }
+});
